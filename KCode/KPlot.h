40a24539b3f53e38843505bdc1e5918ba2f33f5e
Kevin Pedro <kpedro88@gmail.com>
2015-04-15 11:04:47 -0500

    remove leading space before prelim text

--- KCode/KPlot.h
+++ KCode/KPlot.h
@@ -188,9 +188,8 @@ class KPlot{
 			//setup prelim text
 			//todo: add option to enable/disable/change
 			double sizePextra = sizeP - 3; //smaller
-			string prelim_text = " Preliminary";
+			string prelim_text = "Preliminary";
 			globalOpt->Get("prelim_text",prelim_text);
-			if(prelim_text[0] != ' ') prelim_text = " " + prelim_text;
 			TLatex width_test_extra(0,0,prelim_text.c_str());
 			width_test_extra.SetTextSize(sizePextra/pad1H);
 			double uminExtra = umaxCMS;
