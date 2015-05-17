fbb476b43d627707e2b8ac530d3f40b32d247ea4
Kevin Pedro <kpedro88@gmail.com>
2015-05-16 23:39:58 -0500

    add test inputs for KPlotDriver

--- KPlotDriver.C
+++ KPlotDriver.C
@@ -3,7 +3,9 @@
 using namespace std;
 
 //how to run:
-//root -l 'KPlotDriver.C+("root://cmseos.fnal.gov//store/user/pedrok/SUSY2015/Analysis/tree_main","input/input_main_mc_fig.txt")'
+//root -l 'KPlotDriver.C+("root://cmseos.fnal.gov//store/user/pedrok/SUSY2015/Analysis/tree_main","input/input_RA2bin.txt")'
+//test run:
+//root -l 'KPlotDriver.C+("tree_test_main","input/input_RA2bin_test.txt")'
 void KPlotDriver(string dir, string inFile, bool doPrint = false){
 	KPlotManager k(inFile,dir);
 	k.SetPrint(doPrint);
