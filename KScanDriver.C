fad056f5bf88ff65c71a99fbac0dcf5fca32e27b
Kevin Pedro <kpedro88@gmail.com>
2015-10-30 09:45:54 -0500

    remove missing branch error messages from batch drivers

--- KScanDriver.C
+++ KScanDriver.C
@@ -1,9 +1,15 @@
+//custom headers
+#include "KCode/KScanManager.h"
+
+//ROOT headers
+#include <TError.h>
 #include <TROOT.h>
+
+//STL headers
 #include <string>
 #include <sstream>
 #include <iostream>
 #include <vector>
-#include "KCode/KScanManager.h"
 
 using namespace std;
 
@@ -12,10 +18,13 @@ using namespace std;
 //to run interactively:
 //root -b -q -l 'KScanDriver.C+("input/input_scan.txt","T1bbbb_mGluino-1000-1025_mLSP-1to975-1000","root://cmseos.fnal.gov//store/user/pedrok/SUSY2015/Ntuples/RA2sync_Sept23_2015")'
 void KScanDriver(string input="", string setname="", string indir=""){
+	gErrorIgnoreLevel = kBreak;
+	
 	if(input.size()==0){
 		cout << "Recompiled KScanDriver, exiting." << endl;
 		return;
 	}
+	
 	KScanManager k(input,setname,indir);
 	k.Scan();
 }
--- KSkimDriver.C
+++ KSkimDriver.C
@@ -1,9 +1,15 @@
+//custom headers
+#include "KCode/KSkimManager.h"
+
+//ROOT headers
+#include <TError.h>
 #include <TROOT.h>
+
+//STL headers
 #include <string>
 #include <sstream>
 #include <iostream>
 #include <vector>
-#include "KCode/KSkimManager.h"
 
 using namespace std;
 
@@ -12,10 +18,13 @@ using namespace std;
 //to run interactively:
 //root -b -q -l 'KSkimDriver.C+("input/input_selection.txt","T1tttt_1500_100","signal","root://cmseos.fnal.gov//store/user/awhitbe1/PHYS14productionV12","tree_test")'
 void KSkimDriver(string input="", string setname="", string selTypes="", string indir="", string outdir="tree"){
+	gErrorIgnoreLevel = kBreak;
+	
 	if(input.size()==0){
 		cout << "Recompiled KSkimDriver, exiting." << endl;
 		return;
 	}
+	
 	KSkimManager k(input,setname,selTypes,indir,outdir);
 	k.Skim();
 }
