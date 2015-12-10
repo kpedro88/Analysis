55de632e618cad399bdb0b4636d67c6bdda5077f
Kevin Pedro <kpedro88@gmail.com>
2015-12-10 07:19:59 -0600

    split up DC jobs further to reduce memory usage

--- batch/DCsub.sh
+++ batch/DCsub.sh
@@ -6,7 +6,9 @@ KEEPTAR=$1
 JOBDIR=jobs
 INDIR=root://cmseos.fnal.gov//store/user/lpcsusyhad/SusyRA2Analysis2015/Skims/${RUN2PRODV}/scan
 STORE=root://cmseos.fnal.gov//store/user/pedrok/SUSY2015/Analysis/Datacards/${RUN2PRODV}
-SYSTS=nominal,QCD,puunc,pdfunc,scaleunc,isrunc,trigStatUnc,trigSystUnc,JEC,btagSFunc,ctagSFunc,mistagSFunc,btagCFunc,ctagCFunc,mistagCFunc
+SYSTS=nominal,QCD,puunc,pdfunc,scaleunc
+SYSTS2=isrunc,trigStatUnc,trigSystUnc,JEC
+SYSTS3=btagSFunc,ctagSFunc,mistagSFunc,btagCFunc,ctagCFunc,mistagCFunc
 CONTAMS=LDP,GJet_CleanVars,GJetLDP_CleanVars,GJet_CleanVarsGJ,GJetLDP_CleanVarsGJ
 
 ./SKcheck.sh "$KEEPTAR" "$JOBDIR"
@@ -17,5 +19,8 @@ nparts=$(($nparts-1))
 echo "Submitting parts 0.."$nparts
 
 for PART in $(seq 0 $nparts); do
-  ./DCtemp.sh ${JOBDIR} ${INDIR} ${SYSTS} ${CONTAMS} ${PART} ${STORE}
+  ./DCtemp.sh ${JOBDIR} ${INDIR} ${SYSTS} none ${PART} ${STORE}
+  ./DCtemp.sh ${JOBDIR} ${INDIR} ${SYSTS2} none ${PART} ${STORE}
+  ./DCtemp.sh ${JOBDIR} ${INDIR} ${SYSTS3} none ${PART} ${STORE}
+  ./DCtemp.sh ${JOBDIR} ${INDIR} none ${CONTAMS} ${PART} ${STORE}
 done
