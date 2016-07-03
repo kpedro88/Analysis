f95cfcff4327f46f86dedc5dbaa16c7ad2c2eadd
Kevin Pedro <kpedro88@gmail.com>
2016-07-03 18:28:20 -0500

    add more fastsim samples

--- batch/SKsub_fast.sh
+++ batch/SKsub_fast.sh
@@ -4,7 +4,8 @@ source exportProd.sh
 
 JOBDIR=jobs
 INPUT=input/input_selection.txt
-SELTYPE=signal,signal_JECup,signal_JECdown,LDP,SLm,SLe,SLmLDP,SLeLDP,GJet_CleanVars,GJetLDP_CleanVars
+SELTYPE=signal,signal_JECup,signal_JECdown,signal_JERup,signal_JERdown
+SELTYPE2=LDP,SLm,SLe,SLmLDP,SLeLDP,GJet_CleanVars,GJetLDP_CleanVars
 INDIR=root://cmseos.fnal.gov//store/user/lpcsusyhad/SusyRA2Analysis2015/${RUN2PRODV}/scan
 OUTDIR=tree
 STORE=root://cmseos.fnal.gov//store/user/lpcsusyhad/SusyRA2Analysis2015/Skims/${RUN2PRODV}/scan
@@ -25,5 +26,6 @@ source exportFast.sh
 for SAMPLE in ${SAMPLES[@]}
   do
     ./SKtemp.sh ${JOBDIR} ${INPUT} ${SAMPLE} ${SELTYPE} ${INDIR} ${OUTDIR} ${STORE}
+    ./SKtemp.sh ${JOBDIR} ${INPUT} ${SAMPLE} ${SELTYPE2} ${INDIR} ${OUTDIR} ${STORE}
   done
 
