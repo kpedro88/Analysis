af220213da1293075554a70afe15680cfec2295d
Kevin Pedro <kpedro88@gmail.com>
2016-08-23 17:26:17 -0500

    add TChiH fastsim

---
+++ input/dict_scan.py
@@ -0,0 +1,13 @@
+flist = [
+[['Spring16Fast.SMS-T1tttt_TuneCUETP8M1_13TeV-madgraphMLM-pythia8'], ['T1tttt']],
+[['Spring16Fast.SMS-T2bb_TuneCUETP8M1_13TeV-madgraphMLM-pythia8'], ['T2bb']],
+[['Spring16Fast.SMS-T2tt_mStop-400to1200_TuneCUETP8M1_13TeV-madgraphMLM-pythia8'], ['T2tt_mStop-400to1200']],
+[['Spring16Fast.SMS-T1bbbb_TuneCUETP8M1_13TeV-madgraphMLM-pythia8'], ['T1bbbb']],
+[['Spring16Fast.SMS-T2tt_mStop-250to350_TuneCUETP8M1_13TeV-madgraphMLM-pythia8'], ['T2tt_mStop-250to350']],
+[['Spring16Fast.SMS-T2tt_mStop-350to400_TuneCUETP8M1_13TeV-madgraphMLM-pythia8'], ['T2tt_mStop-350to400']],
+[['Spring16Fast.SMS-T2tt_mStop-150to250_TuneCUETP8M1_13TeV-madgraphMLM-pythia8'], ['T2tt_mStop-150to250']],
+[['Spring16Fast.SMS-T1qqqq_TuneCUETP8M1_13TeV-madgraphMLM-pythia8'], ['T1qqqq']],
+[['Spring16Fast.SMS-T5qqqqVV_TuneCUETP8M1_13TeV-madgraphMLM-pythia8'], ['T5qqqqVV']],
+[['Spring16Fast.SMS-T2qq_TuneCUETP8M1_13TeV-madgraphMLM-pythia8'], ['T2qq']],
+[['Spring16Fast.SMS-TChiHH_HToBB_HToBB_TuneCUETP8M1_13TeV-madgraphMLM-pythia8'], ['TChiHH_HToBB_HToBB']],
+]
