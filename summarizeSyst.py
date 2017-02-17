98d74e8364dc5cae9aa4a22a173f1b496c7e078f
Kevin Pedro <kpedro88@gmail.com>
2017-02-17 13:45:15 -0600

    print specified sigfigs for all values

--- summarizeSyst.py
+++ summarizeSyst.py
@@ -1,7 +1,5 @@
 import os
-import subprocess
-from itertools import izip
-import array
+import math
 from optparse import OptionParser
 
 def list_callback(option, opt, value, parser):
@@ -10,6 +8,38 @@ def list_callback(option, opt, value, parser):
 
 def getModel(file):
     return file.split('_')[2]
+
+def printSigFigs(num,fig,maxdec):
+    # use scientific notation to get sig figs, then convert back to regular notation
+    sci = "{0:>.{1}e}".format(num,fig-1)
+    val = sci.split('e')[0]
+    expo = int(sci.split('e')[1])
+    pindex = val.find('.')
+    if expo==0:
+        result = val
+    elif expo>0:
+        if pindex==-1:
+            result = val
+        else:
+            # move decimal point
+            rval = val.replace('.','')
+            pindex2 = pindex+expo
+            if pindex2 >= len(rval): result = rval
+            else: result = rval[0:pindex2]+'.'+rval[pindex2:]
+    else: # expo<0
+        rval = val if pindex==-1 else val.replace('.','')
+        # add leading zeroes
+        result = "0."+'0'*(abs(expo)-1)+rval
+    # recursive truncation
+    if '.' in result and len(result.split('.')[1])>maxdec:
+        if fig==1: 
+            rnum = round(num,abs(expo)-1)
+            if rnum==0.0: return "0.0"
+            else: return printSigFigs(rnum,fig,maxdec)
+        else: return printSigFigs(num,fig-1,maxdec)
+    else:
+        if set(result.replace('.',''))==set([0]): return "0.0"
+        else: return result
 
 def findMinMax(fname,dir,exclude,output):
     model = getModel(fname)
@@ -87,6 +117,8 @@ if __name__ == "__main__":
 
     # make table header
     # range format: xx.xxx--yy.yy
+    sigfig = 2
+    maxdec = 4
     allModels = sorted(models)+["Overall"]
     systRangeLength = 6+2+5
     col0 = "Systematic"
@@ -106,12 +138,14 @@ if __name__ == "__main__":
             hasModel = (model in vals.keys())
             tmin = vals[model][0] if hasModel else 0
             tmax = vals[model][1] if hasModel else 0
-            # don't bother to display a range if values are within 0.01
-            if abs(tmax-tmin)>0.01:
-                trange = "{0:>.3f}--{1:>.2f}".format(tmin,tmax)
-                row += "{0:>{1}} | ".format(trange,columnLengths[im+1])
+            smin = printSigFigs(tmin,sigfig,maxdec)
+            smax = printSigFigs(tmax,sigfig,maxdec)
+            # don't bother to display a range if values are equal within precision
+            if abs(tmax-tmin)>0.0001 and smin != smax:
+                trange = smin+"--"+smax
             else:
-                row += "{0:>{1}.2f} | ".format(tmax,columnLengths[im+1])
+                trange = smax
+            row += "{0:>{1}} | ".format(trange,columnLengths[im+1])
         if syst=="total":
             totalrow = row
             continue
