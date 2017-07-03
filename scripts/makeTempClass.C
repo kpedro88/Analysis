b63b8e9ffc1f05851a3f86b9a5d8b40096edd099
Kevin Pedro <kpedro88@gmail.com>
2017-07-03 14:12:03 -0500

    improve ntuple class creation script error handling

--- scripts/makeTempClass.C
+++ scripts/makeTempClass.C
@@ -2,11 +2,18 @@
 #include <TTree.h>
 
 #include <string>
+#include <stdexcept>
 using std::string;
 
 void makeTempClass(string fname="", string tname="tree", string cname="TempClass"){
 	if(fname.size()==0) return;
 	TFile* file = TFile::Open(fname.c_str());
+	if(!file){
+		throw std::runtime_error("Could not open file: "+fname);
+	}
 	TTree* tree = (TTree*)file->Get(tname.c_str());
+	if(!tree){
+		throw std::runtime_error("Could not get tree: "+tname);
+	}
 	tree->MakeClass(cname.c_str());
 }
