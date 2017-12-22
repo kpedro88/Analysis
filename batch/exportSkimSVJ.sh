2037e2a22f44beb37450a2d031bce61c1d247b54
Kevin Pedro <kpedro88@gmail.com>
2017-12-22 13:36:58 -0600

    fix caps

--- batch/exportSkimSVJ.sh
+++ batch/exportSkimSVJ.sh
@@ -4,14 +4,14 @@ export SAMPLES=(
 SVJ_mZprime-3000_mDark-20_rinv-0.3_alpha-0.1 \
 SVJ_mZprime-3000_mDark-20_rinv-0.3_alpha-0.2 \
 SVJ_mZprime-3000_mDark-20_rinv-0.5_alpha-0.2 \
-SVJ_mZPrime-1000_mDark-20_rinv-0.3_alpha-0.2 \
-SVJ_mZPrime-2000_mDark-20_rinv-0.3_alpha-0.2 \
-SVJ_mZPrime-4000_mDark-20_rinv-0.3_alpha-0.2 \
-SVJ_mZPrime-3000_mDark-1_rinv-0.3_alpha-0.2 \
-SVJ_mZPrime-3000_mDark-50_rinv-0.3_alpha-0.2 \
-SVJ_mZPrime-3000_mDark-100_rinv-0.3_alpha-0.2 \
-SVJ_mZPrime-3000_mDark-20_rinv-0.1_alpha-0.2 \
-SVJ_mZPrime-3000_mDark-20_rinv-0.7_alpha-0.2 \
-SVJ_mZPrime-3000_mDark-20_rinv-0.3_alpha-0.5 \
-SVJ_mZPrime-3000_mDark-20_rinv-0.3_alpha-1 \
+SVJ_mZprime-1000_mDark-20_rinv-0.3_alpha-0.2 \
+SVJ_mZprime-2000_mDark-20_rinv-0.3_alpha-0.2 \
+SVJ_mZprime-4000_mDark-20_rinv-0.3_alpha-0.2 \
+SVJ_mZprime-3000_mDark-1_rinv-0.3_alpha-0.2 \
+SVJ_mZprime-3000_mDark-50_rinv-0.3_alpha-0.2 \
+SVJ_mZprime-3000_mDark-100_rinv-0.3_alpha-0.2 \
+SVJ_mZprime-3000_mDark-20_rinv-0.1_alpha-0.2 \
+SVJ_mZprime-3000_mDark-20_rinv-0.7_alpha-0.2 \
+SVJ_mZprime-3000_mDark-20_rinv-0.3_alpha-0.5 \
+SVJ_mZprime-3000_mDark-20_rinv-0.3_alpha-1 \
 )
