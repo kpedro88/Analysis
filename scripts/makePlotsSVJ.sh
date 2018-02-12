5080d60988315e055d9950e2f4d33ba9a266710d
Kevin Pedro <kpedro88@gmail.com>
2018-02-12 14:25:29 -0600

    ROCs for just MET vars

--- scripts/makePlotsSVJ.sh
+++ scripts/makePlotsSVJ.sh
@@ -20,4 +20,5 @@ for SIG in ${SIGS[@]}; do
 	root -b -l -q 'KPlotDriver.C+(".",{"input/input_svj_rocs_hist_'${SEL}'.txt","input/input_svj_rocs.txt","input/input_svj_rocs_event.txt","input/input_svj_rocs_ext_bkg.txt","'${SIG}'"},{},1)'
 	root -b -l -q 'KPlotDriver.C+(".",{"input/input_svj_rocs_hist_'${SEL}'.txt","input/input_svj_rocs.txt","input/input_svj_rocs_jetpt.txt","input/input_svj_rocs_ext_bkg.txt","'${SIG}'"},{},1)'	
 	root -b -l -q 'KPlotDriver.C+(".",{"input/input_svj_rocs_hist_'${SEL}'.txt","input/input_svj_rocs.txt","input/input_svj_rocs_bothjet.txt","input/input_svj_rocs_ext_bkg.txt","'${SIG}'","input/input_svj_flatten_bothjet.txt"},{},1)'
+#	root -b -l -q 'KPlotDriver.C+(".",{"input/input_svj_rocs_hist_'${SEL}'.txt","input/input_svj_rocs.txt","input/input_svj_rocs_met.txt","input/input_svj_rocs_ext_bkg.txt","'${SIG}'"},{},1)'
 done
