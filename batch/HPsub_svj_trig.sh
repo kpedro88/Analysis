367bda5ba5b3f0cf418c97b36600406ca794f16d
Kevin Pedro <kpedro88@gmail.com>
2018-09-12 16:48:03 -0500

    inputs for trigger studies

---
+++ batch/HPsub_svj_trig.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+source exportProd.sh
+
+JOBDIR=jobs
+INDIR=root://cmseos.fnal.gov//store/user/lpcsusyhad/SVJ2017/${RUN2PRODV}/Skims/tree_dijetmutrig
+STORE=$INDIR/hist
+OUTPUT=
+EXTRA=
+CHECKARGS=""
+
+#check arguments
+while getopts "k" opt; do
+  case "$opt" in
+  k) CHECKARGS="${CHECKARGS} -k"
+    ;;
+  esac
+done
+
+./SKcheck.sh ${CHECKARGS}
+
+for YEAR in 2016 2017; do
+	PERIODS=()
+	if [ "$YEAR" -eq 2016 ]; then
+		PERIODS=(B2 C D E F G H)
+	elif [ "$YEAR" -eq 2017 ]; then
+		PERIODS=(B C D E F)
+	fi
+
+	INPUTS='"input/input_svj_hist_'${YEAR}'.txt","input/input_svj_trig'${YEAR}'.txt","input/input_svj_trig_hist.txt"'
+
+	for PERIOD in ${PERIODS[@]}; do
+		OUTPUT='"OPTION","string:rootfile[hist_trig_'${YEAR}${PERIOD}']"'
+		# name all sets the same for each year in order to hadd
+		EXTRA='"SET","hist\tdata\tSingleMuon_'${YEAR}'","\tbase\tdata\tSingleMuon_'${YEAR}${PERIOD}'\ts:filename[tree_SingleMuon_'${YEAR}${PERIOD}'.root]"'
+		JOBNAME="hist_trig_"${YEAR}${PERIOD}
+		./HPtemp.sh ${JOBDIR} ${INDIR} ${STORE} "$INPUTS" "$OUTPUT" "$EXTRA"
+	done
+done
+
---
+++ batch/SKsub_svj_trig.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+source exportProd.sh
+
+JOBDIR=jobs
+INPUT=input/input_selection_svj.txt
+SELTYPE=dijetmutrig
+INDIR=root://cmseos.fnal.gov//store/user/lpcsusyhad/SusyRA2Analysis2015/${RUN2PRODV}
+OUTDIR=tree
+STORE=root://cmseos.fnal.gov//store/user/lpcsusyhad/SVJ2017/${RUN2PRODV}/Skims
+CHECKARGS=""
+
+#check arguments
+while getopts "k" opt; do
+  case "$opt" in
+  k) CHECKARGS="${CHECKARGS} -k"
+    ;;
+  esac
+done
+
+./SKcheck.sh ${CHECKARGS}
+
+for year in 2016 2017; do
+  source exportSkimData${year}.sh
+  for SAMPLE in ${SAMPLES[@]}; do
+    if [[ $SAMPLE == *"SingleMuon"* ]]; then
+      ./SKtemp.sh ${JOBDIR} ${INPUT} ${SAMPLE} ${SELTYPE} ${INDIR} ${OUTDIR} ${STORE}
+    fi
+  done
+done
