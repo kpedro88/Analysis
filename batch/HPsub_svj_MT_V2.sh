a0f949ee08ab11712dfce08d8ba75b40bcad480a
Kevin Pedro <kpedro88@gmail.com>
2018-09-27 11:52:02 -0500

    more sculpting checks

--- batch/HPsub_svj_MT_V2.sh
+++ batch/HPsub_svj_MT_V2.sh
@@ -24,13 +24,13 @@ STORE=$INDIR/hist
 ./SKcheck.sh ${CHECKARGS}
 
 SETS=../input/input_svj_hp_sets.txt
-INPUTS='"input/input_svj_hist.txt","input/input_svj_mc_cuts.txt"'
+INPUTS='"input/input_svj_hist.txt","input/input_svj_mc_cuts2.txt"'
 for ((i=0; i < $(cat $SETS | wc -l); i+=2)); do
 	LINE1=$(sed -n -e $((i+1))p ${SETS})
 	LINE2=$(sed -n -e $((i+2))p ${SETS})
 	SETNAME=$(echo "$LINE1" | cut -d$'\t' -f3)
 	if [ "$SETNAME" != "QCD" ]; then continue; fi
-	JOBNAME='svj_'${SEL}'_mc_cuts_part'${i}
+	JOBNAME='svj_'${SEL}'_mc_cuts2_part'${i}
 	OUTPUT='"OPTION","string:rootfile['${JOBNAME}']"'
 	EXTRA='"SET","'${LINE1}'","'${LINE2}'"'
 	./HPtemp.sh ${JOBDIR} ${INDIR} ${STORE} "$INPUTS" "$OUTPUT" "$EXTRA" "$JOBNAME"
