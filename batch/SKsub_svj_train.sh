348793bee57140944259c3e62b7d833a8eec8428
Kevin Pedro <kpedro88@gmail.com>
2019-01-24 17:25:07 -0600

    update training ntuples

--- batch/SKsub_svj_train.sh
+++ batch/SKsub_svj_train.sh
@@ -4,52 +4,48 @@ source exportProd.sh
 
 JOBDIR=jobs
 INPUT=input/input_selection_svj_train.txt
-SELTYPE=dijetmthadloose-train-flatsig
-#INDIR=root://cmseos.fnal.gov//store/user/lpcsusyhad/SVJ2017/${RUN2PRODV}/Skims/tree_dijetmthadloose
-INDIR=root://cmseos.fnal.gov//store/user/lpcsusyhad/SVJ2017/ProductionV2/Skims/tree_dijetmthadloose
+SELTYPE=dijetmtdetahadmf-train-flatsig
+INDIR=root://cmseos.fnal.gov//store/user/lpcsusyhad/SVJ2017/${RUN2PRODV}/Skims/tree_dijetmtdetahadmf
 OUTDIR=tree
-STORE=root://cmseos.fnal.gov//store/user/lpcsusyhad/SVJ2017/ProductionV2/Skims
+STORE=root://cmseos.fnal.gov//store/user/lpcsusyhad/SVJ2017/${RUN2PRODV}/Skims
 CHECKARGS=""
 
 #check arguments
 while getopts "k" opt; do
-  case "$opt" in
-  k) CHECKARGS="${CHECKARGS} -k"
-    ;;
-  esac
+	case "$opt" in
+		k) CHECKARGS="${CHECKARGS} -k"
+		;;
+	esac
 done
 
 ./SKcheck.sh ${CHECKARGS}
 
-SAMPLES=(
-QCD_Pt_1000to1400 \
-QCD_Pt_120to170 \
-QCD_Pt_1400to1800 \
-QCD_Pt_170to300 \
-QCD_Pt_1800to2400 \
-QCD_Pt_2400to3200 \
-QCD_Pt_300to470 \
-QCD_Pt_3200toInf \
-QCD_Pt_470to600 \
-QCD_Pt_600to800 \
-QCD_Pt_800to1000 \
-QCD_Pt_80to120 \
-SVJ2_mZprime-1000_mDark-20_rinv-0.3_alpha-0.2 \
-SVJ2_mZprime-2000_mDark-20_rinv-0.3_alpha-0.2 \
-SVJ2_mZprime-3000_mDark-100_rinv-0.3_alpha-0.2 \
-SVJ2_mZprime-3000_mDark-1_rinv-0.3_alpha-0.2 \
-SVJ2_mZprime-3000_mDark-20_rinv-0.1_alpha-0.2 \
-SVJ2_mZprime-3000_mDark-20_rinv-0.3_alpha-0.1 \
-SVJ2_mZprime-3000_mDark-20_rinv-0.3_alpha-0.2 \
-SVJ2_mZprime-3000_mDark-20_rinv-0.3_alpha-0.5 \
-SVJ2_mZprime-3000_mDark-20_rinv-0.3_alpha-1 \
-SVJ2_mZprime-3000_mDark-20_rinv-0.5_alpha-0.2 \
-SVJ2_mZprime-3000_mDark-20_rinv-0.7_alpha-0.2 \
-SVJ2_mZprime-3000_mDark-50_rinv-0.3_alpha-0.2 \
-SVJ2_mZprime-4000_mDark-20_rinv-0.3_alpha-0.2 \
+TYPES=(
+SVJ \
+SVJBkg \
 )
 
-for SAMPLE in ${SAMPLES[@]}
-  do
-    ./SKtemp.sh ${JOBDIR} ${INPUT} ${SAMPLE} ${SELTYPE} ${INDIR} ${OUTDIR} ${STORE}
-  done
+for TYPE in ${TYPES[@]}; do
+	if [ "$TYPE" = SVJ ]; then
+		source exportSkim${TYPE}2017.sh
+	else
+		SAMPLES=(
+QCD_Pt_1000to1400_MC2017 \
+QCD_Pt_120to170_MC2017 \
+QCD_Pt_1400to1800_MC2017 \
+QCD_Pt_170to300_MC2017 \
+QCD_Pt_1800to2400_MC2017 \
+QCD_Pt_2400to3200_MC2017 \
+QCD_Pt_300to470_MC2017 \
+QCD_Pt_3200toInf_MC2017 \
+QCD_Pt_470to600_MC2017 \
+QCD_Pt_600to800_MC2017 \
+QCD_Pt_800to1000_MC2017 \
+QCD_Pt_80to120_MC2017 \
+		)
+	fi
+	
+	for SAMPLE in ${SAMPLES[@]}; do
+		./SKtemp.sh ${JOBDIR} ${INPUT} ${SAMPLE} ${SELTYPE} ${INDIR} ${OUTDIR} ${STORE}
+	done
+done
---
+++ scripts/makeSVJInputs.py
@@ -0,0 +1,98 @@
+import os
+from argparse import ArgumentParser
+from collections import OrderedDict
+
+def parse(sample):
+    params = {}
+    samplesplit = sample.split("_")
+    for s in samplesplit:
+        if "-" in s:
+            kv = s.split("-")
+            params[kv[0]] = kv[1]
+
+    return params
+
+# define options
+parser = ArgumentParser()
+parser.add_argument("-d", "--dir", dest="dir", default="/store/user/lpcsusyhad/SVJ2017/Run2ProductionV16/Skims/tree_dijetmtdetahadmf/", help="location of root files (LFN)")
+args = parser.parse_args()
+
+# find the root files
+files = filter(None,os.popen("xrdfs root://cmseos.fnal.gov/ ls "+args.dir).read().split('\n'))
+# basename
+files = [ f.split("/")[-1] for f in files]
+files = [x for x in files if "SVJ" in x]
+
+# style
+colors = ["kBlack","kBlue","kMagenta + 2","kRed","kCyan + 2","kMagenta","kOrange + 7","kYellow + 3","kGreen + 2","kPink - 9","kGray + 1"]
+styles = [1,2,3,4,7,9]
+cc = 0
+cs = 0
+
+# alpha val translation
+alphavals = {
+    "high": -1,
+    "peak": -2,
+    "low": -3,
+}
+
+# default params
+params_default = OrderedDict([
+    ("mZprime", 3000),
+    ("mDark", 20),
+    ("rinv", 0.3),
+    ("alpha", "peak"),
+])
+
+# open files
+dfile = open("input/input_svj_hp_sets_sig.txt",'w')
+sfile = open("input/input_svj_sets_sig.txt",'w')
+sfile.write("SET\n")
+ifile = open("input/input_svj_train_options.txt",'w')
+numers = []
+branches = []
+for sample in files:
+    # parse filename: tree_SVJ_mZprime-1000_mDark-20_rinv-0.3_alpha-peak_MC2017.root
+    params = parse(sample)
+    name = sample.replace("tree_","").replace(".root","")
+    # make short name
+    sname = "SVJ"+"_"+str(params["mZprime"])+"_"+str(params["mDark"])+"_"+str(params["rinv"])+"_"+str(params["alpha"])
+    year = "MC2017" if "MC2017" in sample else "MC2016"
+    # make flat branch name
+    found_nondefault = False
+    for p in params_default:
+        pv = params[p]
+        if str(params_default[p]) != str(pv):
+            if p=="mZprime": branches.append("Z"+str(int(float(pv)/100)))
+            elif p=="mDark": branches.append("D"+str(pv))
+            elif p=="rinv": branches.append("R"+str(int(float(pv)*10)))
+            elif p=="alpha":
+                pv2 = "Pk" if pv=="peak" else "Hi" if pv=="high" else "Lo" if pv=="low" else str(int(float(pv)*10))
+                branches.append("A"+pv2)
+            found_nondefault = True
+            break
+    if not found_nondefault: # default
+        branches.append("Z"+str(int(float(params["mZprime"])/100)))
+    # make lines
+    dline1 = "\t".join(["hist", "mc", sname+"_"+year, "s:legname["+sname+"]","c:color["+colors[cc]+"]","i:linestyle["+str(styles[cs])+"]"])
+    alphaval = alphavals[params["alpha"]] if params["alpha"] in alphavals else params["alpha"]
+    dline2 = "\t".join(["", "base", "mc", sname+"_"+year, "s:filename["+sample+"]","d:mZprime["+str(params["mZprime"])+"]","d:mDark["+str(params["mDark"])+"]","d:rinv["+str(params["rinv"])+"]","d:alpha["+str(alphaval)+"]"])
+    dfile.write(dline1+"\n")
+    dfile.write(dline2+"\n")
+    # make skim line
+    sline = "\t".join(["base", "skim", name, "s:filename["+sample+"]","b:data[0]","s:flatsuff["+sname+"_"+year+"]"])
+    sfile.write(sline+"\n")
+    # append to training lines
+    numers.append(sname+"_"+year)
+    
+    # increment style counters
+    cc += 1
+    if cc>=len(colors):
+        cc = 0
+        cs += 1
+        if cs>=len(styles):
+            cs = 0
+
+# write options
+ifile.write("vs:flatnumers["+",".join(numers)+"]"+"\n")
+ifile.write("vs:flatbranches["+",".join(branches)+"]"+"\n")
