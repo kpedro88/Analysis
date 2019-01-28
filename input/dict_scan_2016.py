e25484708ad116ba70ede286805aecc577367907
Kevin Pedro <kpedro88@gmail.com>
2019-01-28 11:40:35 -0600

    update scan lists

---
+++ input/dict_scan_2016.py
@@ -0,0 +1,4 @@
+flist = [
+[['Summer16v3Fast.SMS-T1tttt_TuneCUETP8M1_13TeV-madgraphMLM-pythia8'], ['T1tttt_MC2016']],
+[['Summer16v3Fast.SMS-T2tt_mStop-150to250_TuneCUETP8M1_13TeV-madgraphMLM-pythia8'], ['T2tt_mStop-150to250_MC2016']],
+]
---
+++ input/dict_scan_2017.py
@@ -0,0 +1,3 @@
+flist = [
+[['Fall17Fast.SMS-T1tttt_TuneCP2_13TeV-madgraphMLM-pythia8'], ['T1tttt_MC2017']],
+]
