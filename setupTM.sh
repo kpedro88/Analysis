113c12334df97207b2d0f0d0e732ff77c89cf05d
Kevin Pedro <kpedro88@gmail.com>
2019-01-30 15:00:44 -0600

    fix setup

--- setupTM.sh
+++ setupTM.sh
@@ -8,4 +8,3 @@ cd $CMSSW_BASE/src/TreeMaker/Reflex
 scram b -j 8
 cd $CMSSW_BASE/lib/$SCRAM_ARCH/
 mv TreeMakerReflex_xr.rootmap TreeMakerReflex.rootmap
-mv TreeMakerReflex_xr_rdict.pcm TreeMakerReflex_rdict.pcm
