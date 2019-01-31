c5a7a1513c172383e7ff8f81b6c8c80099005003
Kevin Pedro <kpedro88@gmail.com>
2019-01-31 02:27:51 -0600

    update dc script

--- batch/DCsub.sh
+++ batch/DCsub.sh
@@ -5,47 +5,54 @@ source exportProd.sh
 JOBDIR=jobs
 INDIR=root://cmseos.fnal.gov//store/user/lpcsusyhad/SusyRA2Analysis2015/Skims/${RUN2PRODV}/scan
 STORE=root://cmseos.fnal.gov//store/user/pedrok/SUSY2015/Analysis/Datacards/${RUN2PRODV}
-SYSTS=nominal,scaleuncUp,scaleuncDown,isruncUp,isruncDown,triguncUp,triguncDown,btagSFuncUp,btagSFuncDown,mistagSFuncUp,mistagSFuncDown,btagCFuncUp,btagCFuncDown,ctagCFuncUp,ctagCFuncDown,mistagCFuncUp,mistagCFuncDown,isotrackuncUp,isotrackuncDown,lumiuncUp,lumiuncDown,jetiduncUp,jetiduncDown,puaccuncUp,puaccuncDown
+#SYSTS=nominal,scaleuncUp,scaleuncDown,isruncUp,isruncDown,triguncUp,triguncDown,btagSFuncUp,btagSFuncDown,mistagSFuncUp,mistagSFuncDown,btagCFuncUp,btagCFuncDown,ctagCFuncUp,ctagCFuncDown,mistagCFuncUp,mistagCFuncDown,isotrackuncUp,isotrackuncDown,lumiuncUp,lumiuncDown,jetiduncUp,jetiduncDown,puaccuncUp,puaccuncDown
+SYSTS=nominal,scaleuncUp,scaleuncDown,isruncUp,isruncDown,btagSFuncUp,btagSFuncDown,mistagSFuncUp,mistagSFuncDown,isotrackuncUp,isotrackuncDown,lumiuncUp,lumiuncDown,jetiduncUp,jetiduncDown,prefireuncUp,prefireuncDown
 VARS=JECup,JECdown,JERup,JERdown,genMHT
 CHECKARGS=""
 SUFFIX=""
 SKIPFILE=""
+YEARS=()
 
 #check arguments
-while getopts "kx:s:" opt; do
-  case "$opt" in
-  k) CHECKARGS="${CHECKARGS} -k"
-    ;;
-  x) SUFFIX=$OPTARG
-    ;;
-  s) SKIPFILE=$OPTARG
+while getopts "kx:y:s:" opt; do
+	case "$opt" in
+		k) CHECKARGS="${CHECKARGS} -k"
+		;;
+		x) SUFFIX=$OPTARG
+		;;
+		y) IFS="," read -a YEARS <<< "$OPTARG"
+		;;
+		s) SKIPFILE=$OPTARG
+		;;
   esac
 done
 
-if [ -n "SKIPFILE" ]; then
-  source ${SKIPFILE}
-fi
-
-if [ -n "$SUFFIX" ]; then
-  source exportSkimFast${SUFFIX}.sh
-else
-  source exportSkimFast.sh
-fi
-
 ./SKcheck.sh ${CHECKARGS}
 
-for SAMPLE in ${SAMPLES[@]}; do
-  # check skips
-  SKIPTHIS=""
-  for SKIP in ${SKIPLIST[@]}; do
-    if [[ $SAMPLE == "$SKIP"* ]]; then
-      SKIPTHIS=yes
-      break
-    fi
-  done
-  if [[ -n "$SKIPTHIS" ]]; then
-    continue
-  fi
+if [ -n "$SKIPFILE" ]; then
+	source ${SKIPFILE}
+fi
+
+for YEAR in ${YEARS[@]}; do
+	if [ -n "$SUFFIX" ]; then
+		source exportSkimFast${SUFFIX}${YEAR}.sh
+	else
+		source exportSkimFast${YEAR}.sh
+	fi
+
+	for SAMPLE in ${SAMPLES[@]}; do
+		# check skips
+		SKIPTHIS=""
+		for SKIP in ${SKIPLIST[@]}; do
+			if [[ $SAMPLE == "$SKIP"* ]]; then
+				SKIPTHIS=yes
+				break
+			fi
+		done
+		if [[ -n "$SKIPTHIS" ]]; then
+			continue
+		fi
   
-  ./DCtemp.sh ${JOBDIR} ${INDIR} ${SYSTS} ${VARS} ${STORE} ${SAMPLE}
+		./DCtemp.sh ${JOBDIR} ${INDIR} ${SYSTS} ${VARS} ${STORE} ${SAMPLE}
+	done
 done
