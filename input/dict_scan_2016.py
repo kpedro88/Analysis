74e624843adae6b0e7cc26f0a9a2de77a36479f3
Kevin Pedro <kpedro88@gmail.com>
2019-02-17 13:56:37 -0600

    update fastsim scan lists

--- input/dict_scan_2016.py
+++ input/dict_scan_2016.py
@@ -1,4 +1,15 @@
 flist = [
+[['Summer16v3Fast.SMS-T1bbbb_TuneCUETP8M1_13TeV-madgraphMLM-pythia8'], ['T1bbbb_MC2016']],
+[['Summer16v3Fast.SMS-T1qqqq_TuneCUETP8M1_13TeV-madgraphMLM-pythia8'], ['T1qqqq_MC2016']],
 [['Summer16v3Fast.SMS-T1tttt_TuneCUETP8M1_13TeV-madgraphMLM-pythia8'], ['T1tttt_MC2016']],
+[['Summer16v3Fast.SMS-T2bb_TuneCUETP8M1_13TeV-madgraphMLM-pythia8'], ['T2bb_MC2016']],
+[['Summer16v3Fast.SMS-T2bb_mSbot-1650to2600_TuneCUETP8M1_13TeV-madgraphMLM-pythia8'], ['T2bb_mSbot-1650to2600_MC2016']],
+[['Summer16v3Fast.SMS-T2qq_TuneCUETP8M1_13TeV-madgraphMLM-pythia8'], ['T2qq_MC2016']],
+[['Summer16v3Fast.SMS-T2qq_mSq-1850to2600_TuneCUETP8M1_13TeV-madgraphMLM-pythia8'], ['T2qq_mSq-1850to2600_MC2016']],
+[['Summer16v3Fast.SMS-T2tt_mStop-1200to2000_TuneCUETP8M1_13TeV-madgraphMLM-pythia8'], ['T2tt_mStop-1200to2000_MC2016']],
 [['Summer16v3Fast.SMS-T2tt_mStop-150to250_TuneCUETP8M1_13TeV-madgraphMLM-pythia8'], ['T2tt_mStop-150to250_MC2016']],
+[['Summer16v3Fast.SMS-T2tt_mStop-250to350_TuneCUETP8M1_13TeV-madgraphMLM-pythia8'], ['T2tt_mStop-250to350_MC2016']],
+[['Summer16v3Fast.SMS-T2tt_mStop-350to400_TuneCUETP8M1_13TeV-madgraphMLM-pythia8'], ['T2tt_mStop-350to400_MC2016']],
+[['Summer16v3Fast.SMS-T2tt_mStop-400to1200_TuneCUETP8M1_13TeV-madgraphMLM-pythia8'], ['T2tt_mStop-400to1200_MC2016']],
+[['Summer16v3Fast.SMS-T5qqqqVV_TuneCUETP8M1_13TeV-madgraphMLM-pythia8'], ['T5qqqqVV_MC2016']],
 ]
--- input/dict_scan_2017.py
+++ input/dict_scan_2017.py
@@ -1,3 +1,15 @@
 flist = [
-[['Fall17Fast.SMS-T1tttt_TuneCP2_13TeV-madgraphMLM-pythia8'], ['T1tttt_MC2017']],
+[['Fall17Fast.SMS-T1bbbb_TuneCP2_13TeV-madgraphMLM-pythia8', 'Fall17Fast.SMS-T1bbbb_TuneCP2_13TeV-madgraphMLM-pythia8_ext1'], ['T1bbbb_MC2017']],
+[['Fall17Fast.SMS-T1qqqq_TuneCP2_13TeV-madgraphMLM-pythia8', 'Fall17Fast.SMS-T1qqqq_TuneCP2_13TeV-madgraphMLM-pythia8_ext1'], ['T1qqqq_MC2017']],
+[['Fall17Fast.SMS-T1tttt_TuneCP2_13TeV-madgraphMLM-pythia8', 'Fall17Fast.SMS-T1tttt_TuneCP2_13TeV-madgraphMLM-pythia8_ext1'], ['T1tttt_MC2017']],
+[['Fall17Fast.SMS-T2bb_TuneCP2_13TeV-madgraphMLM-pythia8', 'Fall17Fast.SMS-T2bb_TuneCP2_13TeV-madgraphMLM-pythia8_ext1'], ['T2bb_MC2017']],
+[['Fall17Fast.SMS-T2bb_mSbot-1650to2600_TuneCP2_13TeV-madgraphMLM-pythia8', 'Fall17Fast.SMS-T2bb_mSbot-1650to2600_TuneCP2_13TeV-madgraphMLM-pythia8_ext1'], ['T2bb_mSbot-1650to2600_MC2017']],
+[['Fall17Fast.SMS-T2qq_TuneCP2_13TeV-madgraphMLM-pythia8', 'Fall17Fast.SMS-T2qq_TuneCP2_13TeV-madgraphMLM-pythia8_ext1'], ['T2qq_MC2017']],
+[['Fall17Fast.SMS-T2qq_mSq-1850to2600_TuneCP2_13TeV-madgraphMLM-pythia8', 'Fall17Fast.SMS-T2qq_mSq-1850to2600_TuneCP2_13TeV-madgraphMLM-pythia8_ext1'], ['T2qq_mSq-1850to2600_MC2017']],
+[['Fall17Fast.SMS-T2tt_mStop-1200to2000_TuneCP2_13TeV-madgraphMLM-pythia8', 'Fall17Fast.SMS-T2tt_mStop-1200to2000_TuneCP2_13TeV-madgraphMLM-pythia8_ext1'], ['T2tt_mStop-1200to2000_MC2017']],
+[['Fall17Fast.SMS-T2tt_mStop-150to250_TuneCP2_13TeV-madgraphMLM-pythia8', 'Fall17Fast.SMS-T2tt_mStop-150to250_TuneCP2_13TeV-madgraphMLM-pythia8_ext1'], ['T2tt_mStop-150to250_MC2017']],
+[['Fall17Fast.SMS-T2tt_mStop-250to350_TuneCP2_13TeV-madgraphMLM-pythia8', 'Fall17Fast.SMS-T2tt_mStop-250to350_TuneCP2_13TeV-madgraphMLM-pythia8_ext1'], ['T2tt_mStop-250to350_MC2017']],
+[['Fall17Fast.SMS-T2tt_mStop-350to400_TuneCP2_13TeV-madgraphMLM-pythia8', 'Fall17Fast.SMS-T2tt_mStop-350to400_TuneCP2_13TeV-madgraphMLM-pythia8_ext1'], ['T2tt_mStop-350to400_MC2017']],
+[['Fall17Fast.SMS-T2tt_mStop-400to1200_TuneCP2_13TeV-madgraphMLM-pythia8', 'Fall17Fast.SMS-T2tt_mStop-400to1200_TuneCP2_13TeV-madgraphMLM-pythia8_ext1'], ['T2tt_mStop-400to1200_MC2017']],
+[['Fall17Fast.SMS-T5qqqqVV_TuneCP2_13TeV-madgraphMLM-pythia8', 'Fall17Fast.SMS-T5qqqqVV_TuneCP2_13TeV-madgraphMLM-pythia8_ext1'], ['T5qqqqVV_MC2017']],
 ]
