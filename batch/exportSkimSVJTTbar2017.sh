925c90e0e29c6d5d29ddfb1eb725ebe3b8204878
Kevin Pedro <kpedro88@gmail.com>
2019-05-10 17:07:56 -0500

    update bdt training settings

---
+++ batch/exportSkimSVJTTbar2017.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+export SAMPLES=(
+TTJets \
+TTJets_DiLept \
+TTJets_DiLept_genMET150 \
+TTJets_SingleLeptFromT \
+TTJets_SingleLeptFromT_genMET150 \
+TTJets_SingleLeptFromTbar \
+TTJets_SingleLeptFromTbar_genMET150 \
+TTJets_HT600to800 \
+TTJets_HT800to1200 \
+TTJets_HT1200to2500 \
+TTJets_HT2500toInf \
+)
