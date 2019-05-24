f04b5196fb283b322ada4405990adb2cb287283d
Kevin Pedro <kpedro88@gmail.com>
2019-05-24 14:50:53 -0500

    Squashed commit of the following:
    commit a81a10b24ebce34357b4d5d5138517a75535d573
    Author: Kevin Pedro <kpedro88@gmail.com>
    Date:   Fri May 24 14:38:52 2019 -0500
    
        access trees by reference
    
    commit a71596083bfa19b896990645ed6d1622bd5036e5
    Author: Kevin Pedro <kpedro88@gmail.com>
    Date:   Fri May 24 13:48:38 2019 -0500
    
        avoid ptrs to feature values
    
    commit ff4dc2ba5bfcfd2fc1139a16d00e42cab292c862
    Author: Kevin Pedro <kpedro88@gmail.com>
    Date:   Fri May 24 13:35:32 2019 -0500
    
        exact fix from cmssw PR 13143
    
    commit cab2d4b5577861b8414841c3497f674521471b49
    Author: Kevin Pedro <kpedro88@gmail.com>
    Date:   Fri May 24 11:44:00 2019 -0500
    
        change memory locality
    
    commit 2909964aea1b6d904c54761fa710ed8ea48284fe
    Author: Kevin Pedro <kpedro88@gmail.com>
    Date:   Fri May 24 11:25:01 2019 -0500
    
        fix parsing, add some debug statements
    
    commit e3fbaab982bb1cc3f6d488851f92ded8d4bb861e
    Author: Kevin Pedro <kpedro88@gmail.com>
    Date:   Fri May 24 10:16:04 2019 -0500
    
        try in-place tree

--- KMVA/BDTree.h
+++ KMVA/BDTree.h
@@ -5,31 +5,38 @@
 #include <string>
 #include <unordered_map>
 #include <algorithm>
+#include <iterator>
+#include <iostream>
 #include <cmath>
 #include <exception>
 
 #define PUGIXML_HEADER_ONLY
 #include "pugixml.hpp"
 
+struct DNode {
+	DNode(unsigned i, double c, int l=0, int r=0) : index_(i), cut_(c), left_(l), right_(r) {}
+	unsigned index_;
+	double cut_;
+	int left_;
+	int right_;
+};
+
+//now based on https://github.com/cms-sw/cmssw/blob/master/CondFormats/EgammaObjects/interface/GBRTree.h
 class DTree {
 	public:
-		DTree(double d, unsigned i=0) : left_(nullptr), right_(nullptr), index_(i), cut_(d), isLeaf_(true) {}
+		DTree() {}
 		virtual ~DTree() {}
-		double decision(const vector<float>& features) const {
-			return isLeaf_ ? cut_ :
-				features[index_] <= cut_ ? left_->decision(features) : right_->decision(features);
+		inline double decision(const vector<float>& features) const {
+			int index = 0;
+			do {
+				auto l = nodes_[index].left_;
+				auto r = nodes_[index].right_;
+				index = features[nodes_[index].index_] <= nodes_[index].cut_ ? l : r;
+			} while (index>0);
+			return vres_[-index];
 		}
-		void setChildren(DTree* left, DTree* right) {
-			left_ = left;
-			right_ = right;
-			if(left_ and right_) isLeaf_ = false;
-		}
-	protected:
-		DTree* left_;
-		DTree* right_;
-		unsigned index_;
-		double cut_;
-		bool isLeaf_;
+		std::vector<DNode> nodes_;
+		std::vector<double> vres_;
 };
 
 class BDTree {
@@ -50,13 +57,23 @@ class BDTree {
 
 			//set up trees
 			const auto& weights = method.child("Weights");
-			trees_.reserve(weights.attribute("NTrees").as_int());
+			trees_.resize(weights.attribute("NTrees").as_int());
+			unsigned nt = 0;
 			for(const auto& btree : weights.children("BinaryTree")){
 				size_t nc = nChildren(btree,"Node");
 				if(nc!=1) throw std::runtime_error("BinaryTree should have 1 root node, but has "+std::to_string(nc));
 
 				//start parsing from the root
-				trees_.push_back(parseTree(btree.child("Node")));
+				parseTree(btree.child("Node"),trees_[nt]);
+/*
+				std::cout << "vindex_ = "; std::copy(trees_[nt].nodes_.index_.begin(),trees_[nt].nodes_.index_.end(),std::ostream_iterator<unsigned>(std::cout,",")); std::cout << std::endl;
+				std::cout << "vcut_ = "; std::copy(trees_[nt].nodes_.cut_.begin(),trees_[nt].nodes_.cut_.end(),std::ostream_iterator<double>(std::cout,",")); std::cout << std::endl;
+				std::cout << "vleft_ = "; std::copy(trees_[nt].nodes_.left_.begin(),trees_[nt].nodes_.left_.end(),std::ostream_iterator<int>(std::cout,",")); std::cout << std::endl;
+				std::cout << "vright_ = "; std::copy(trees_[nt].nodes_.right_.begin(),trees_[nt].nodes_.right_.end(),std::ostream_iterator<int>(std::cout,",")); std::cout << std::endl;
+				std::cout << "vres_ = "; std::copy(trees_[nt].vres_.begin(),trees_[nt].vres_.end(),std::ostream_iterator<double>(std::cout,",")); std::cout << std::endl;
+				std::cout << std::endl;
+*/
+				++nt;
 			}
 		}
 		
@@ -67,8 +84,8 @@ class BDTree {
 		}
 		double evaluate(){
 			double sum = 0.;
-			for(auto tree : trees_){
-				sum += tree->decision(features_);
+			for(const auto& tree : trees_){
+				sum += tree.decision(features_);
 			}
 			//use dumb convention from TMVA::MethodBDT::GetGradBoostMVA() for consistency
 			return 2.0/(1.0+exp(-2.0*sum))-1;
@@ -80,29 +97,38 @@ class BDTree {
 		size_t nChildren(const pugi::xml_node& node, std::string cname) const {
 			return std::distance(node.children(cname.c_str()).begin(),node.children(cname.c_str()).end());
 		}
-		DTree* parseTree(const pugi::xml_node& node) const {
-			DTree* result = nullptr;
+		bool isLeaf(const pugi::xml_node& node) const {
 			size_t nc = nChildren(node,"Node");
+			return nc==0;
+		}
+		void parseTree(const pugi::xml_node& node, DTree& tree) const {
 			//leaf case
-			if(nc==0){
-				result = new DTree(node.attribute("res").as_double());
+			if(isLeaf(node)){
+				tree.vres_.push_back(node.attribute("res").as_double());
 			}
 			//tree case
 			else {
-				result = new DTree(node.attribute("Cut").as_double(),node.attribute("IVar").as_int());
-				std::array<DTree*,2> children{nullptr,nullptr};
-				//now parse children recursively
-				unsigned cindex = 0;
+				int thisidx = tree.nodes_.size();
+				tree.nodes_.emplace_back(node.attribute("IVar").as_int(),node.attribute("Cut").as_double());
+
+				//get children
+				pugi::xml_node left;
+				pugi::xml_node right;
 				for(const auto& cnode : node.children("Node")){
-					children[cindex] = parseTree(cnode);
-					++cindex;
+					std::string pos = cnode.attribute("pos").as_string();
+					if(pos.compare("l")==0) left = cnode;
+					else if(pos.compare("r")==0) right = cnode;
 				}
-				result->setChildren(children[0],children[1]);
+
+				//now parse children recursively
+				tree.nodes_[thisidx].left_ = isLeaf(left) ? -tree.vres_.size() : tree.nodes_.size();
+				parseTree(left,tree);
+				tree.nodes_[thisidx].right_ = isLeaf(right) ? -tree.vres_.size() : tree.nodes_.size();
+				parseTree(right,tree);
 			}
-			return result;
 		}
 	
-		std::vector<DTree*> trees_;
+		std::vector<DTree> trees_;
 		std::unordered_map<std::string,unsigned> feature_indices_;
 		std::vector<float> features_;
 };
