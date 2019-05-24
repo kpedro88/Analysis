4436970d48f0cbd66618d28aaf75f0ae11687008
Kevin Pedro <kpedro88@gmail.com>
2019-05-24 10:17:14 -0500

    combine headers

--- KMVA/BDTree.h
+++ KMVA/BDTree.h
@@ -1,8 +1,6 @@
 #ifndef BDTREE_H
 #define BDTREE_H
 
-#include "DTree.h"
-
 #include <vector>
 #include <string>
 #include <unordered_map>
@@ -12,6 +10,27 @@
 
 #define PUGIXML_HEADER_ONLY
 #include "pugixml.hpp"
+
+class DTree {
+	public:
+		DTree(double d, unsigned i=0) : left_(nullptr), right_(nullptr), index_(i), cut_(d), isLeaf_(true) {}
+		virtual ~DTree() {}
+		double decision(const vector<float*>& features) const {
+			return isLeaf_ ? cut_ :
+				*(features[index_]) <= cut_ ? left_->decision(features) : right_->decision(features);
+		}
+		void setChildren(DTree* left, DTree* right) {
+			left_ = left;
+			right_ = right;
+			if(left_ and right_) isLeaf_ = false;
+		}
+	protected:
+		DTree* left_;
+		DTree* right_;
+		unsigned index_;
+		double cut_;
+		bool isLeaf_;
+};
 
 class BDTree {
 	public:
