9af98e3ddaff76ceae82b265cf79cc8be1c17972
Kevin Pedro <kpedro88@gmail.com>
2019-12-14 16:23:18 -0600

    use full sel

--- scripts/runSVJ.sh
+++ scripts/runSVJ.sh
@@ -3,7 +3,7 @@
 function discrs(){
 	CONFIG=$1
 	for MVA in ${BDTNAMES[@]}; do
-		root -b -l -q 'KPlotDriver.C+("root://cmseos.fnal.gov//store/user/lpcsusyhad/SVJ2017/Run2ProductionV17/Skims/tree_dijetmtdetahadloosemf",{"input/input_svj_stack_dijetmtdetahad_2017.txt","input/input_svj_sel_'${SELBASE}${CONFIG}'.txt","input/input_svj_sel_bdt_incl.txt","input/input_svj_sel_full_bdt_regions.txt","input/input_svj_discr_full_bdtnew_regions.txt","input/input_svj_sets_full_bdt_regions.txt"},{"OPTION","vs:selections[pre-'${MVA}']","s+:printsuffix[_'${CONFIG}'_'${MVA}'_stack]","s:rootfile[test/bothjetAK8bdt__dijetmtdetahad_'${CONFIG}'_'${MVA}']","vs+:printformat[pdf]"},1)' >& log_discr_${MVA}_${CONFIG}.log
+		root -b -l -q 'KPlotDriver.C+("root://cmseos.fnal.gov//store/user/lpcsusyhad/SVJ2017/Run2ProductionV17/Skims/tree_dijetmtdetahadloosefull",{"input/input_svj_stack_dijetmtdetahad_2017.txt","input/input_svj_sel_'${SELBASE}${CONFIG}'.txt","input/input_svj_sel_bdt_incl.txt","input/input_svj_sel_full_bdt_regions.txt","input/input_svj_discr_full_bdtnew_regions.txt","input/input_svj_sets_full_bdt_regions.txt"},{"OPTION","vs:selections[pre-'${MVA}']","s+:printsuffix[_'${CONFIG}'_'${MVA}'_stack]","s:rootfile[test/bothjetAK8bdt__dijetmtdetahad_'${CONFIG}'_'${MVA}']","vs+:printformat[pdf]"},1)' >& log_discr_${MVA}_${CONFIG}.log
 		# make ROC curves
 		root -b -l -q 'KPlotDriver.C+(".",{"input/input_svj_rocs_hist_dijetmtdetahadloosemf.txt","input/input_svj_roc_bdt_ext.txt"},{"OPTION","s:extfilename[test/bothjetAK8bdt__dijetmtdetahad_'${CONFIG}'_'${MVA}'.root]","vs+:printformat[pdf]","s:rootfile[test/bothjetAK8bdt__dijetmtdetahad_'${CONFIG}'_'${MVA}'_roc]","s+:rocsuffix[_'${CONFIG}'_'${MVA}']"},1)';
 	done
@@ -13,14 +13,14 @@ function discrs(){
 function mt(){
 	CONFIG=$1
 	for MVA in ${BDTNAMES[@]}; do
-		root -b -l -q 'KPlotDriver.C+("root://cmseos.fnal.gov//store/user/lpcsusyhad/SVJ2017/Run2ProductionV17/Skims/tree_dijetmtdetahadloosemf",{"input/input_svj_stack_dijetmtdetahad_2017.txt","input/input_svj_sel_'${SELBASE}${CONFIG}'.txt","input/input_svj_sel_bdt2.txt","input/input_svj_sel_full_bdt_regions.txt","input/input_svj_mt_full_bdt_regions.txt","input/input_svj_sets_full_bdt_regions.txt"},{"OPTION","s+:printsuffix[_'$CONFIG'_S-'$MVA'2]","vs:selections[S-'$MVA']","vs:extra_text[S-'$MVA'2 ('$CONFIG')]"},1)' >& log_mt_S-${MVA}2_${CONFIG}.log
+		root -b -l -q 'KPlotDriver.C+("root://cmseos.fnal.gov//store/user/lpcsusyhad/SVJ2017/Run2ProductionV17/Skims/tree_dijetmtdetahadloosefull",{"input/input_svj_stack_dijetmtdetahad_2017.txt","input/input_svj_sel_'${SELBASE}${CONFIG}'.txt","input/input_svj_sel_bdt2.txt","input/input_svj_sel_full_bdt_regions.txt","input/input_svj_mt_full_bdt_regions.txt","input/input_svj_sets_full_bdt_regions.txt"},{"OPTION","s+:printsuffix[_'$CONFIG'_S-'$MVA'2]","vs:selections[S-'$MVA']","vs:extra_text[S-'$MVA'2 ('$CONFIG')]"},1)' >& log_mt_S-${MVA}2_${CONFIG}.log
 	done
 }
 
 # this makes the MT distribution from the split full region histograms
 function regions(){
 	CONFIG=$1
-	root -b -l -q 'KPlotDriver.C+("root://cmseos.fnal.gov//store/user/lpcsusyhad/SVJ2017/Run2ProductionV17/Skims/tree_dijetmtdetahadloosemf",{"input/input_svj_stack_generic.txt","input/input_svj_sel_'${SELBASE}${CONFIG}'.txt","input/input_svj_sel_bdt_incl.txt","input/input_svj_sel_full_bdt_regions.txt","input/input_svj_mt_fullnew_hist.txt"},{"OPTION","s:rootfile[test/MTAK8_dijetmtdetahadloosemf_fullbdtregions_'${CONFIG}']","vs:selections['${BDTSELS}']"},0)' >& log_fullbdtregions${CONFIG}.log
+	root -b -l -q 'KPlotDriver.C+("root://cmseos.fnal.gov//store/user/lpcsusyhad/SVJ2017/Run2ProductionV17/Skims/tree_dijetmtdetahadloosefull",{"input/input_svj_stack_generic.txt","input/input_svj_sel_'${SELBASE}${CONFIG}'.txt","input/input_svj_sel_bdt_incl.txt","input/input_svj_sel_full_bdt_regions.txt","input/input_svj_mt_fullnew_hist.txt"},{"OPTION","s:rootfile[test/MTAK8_dijetmtdetahadloosemf_fullbdtregions_'${CONFIG}']","vs:selections['${BDTSELS}']"},0)' >& log_fullbdtregions${CONFIG}.log
 	python splitRegionsSVJ.py test/MTAK8_dijetmtdetahadloosemf_fullbdtregions_${CONFIG}.root
 	for MVA in ${BDTNAMES[@]}; do
 		root -b -l -q 'KPlotDriver.C+(".",{"input/input_svj_stack_dijetmtdetahad_2017.txt","input/input_svj_mt_full_bdt_regions.txt","input/input_svj_ext_full_'${MVA}'_regions.txt"},{"OPTION","s:extfilename[test/MTAK8_dijetmtdetahadloosemf_fullbdtregions_'${CONFIG}'_split.root]","s:exthisto_extra[S-'$MVA'2]","s+:printsuffix[_'$CONFIG'_S-'$MVA'2]","vs:extra_text[S-'$MVA'2 ('$CONFIG')]","vs+:printformat[pdf]"},1)' >& log_mt_S-${MVA}2_${CONFIG}.log
