2a886d97acbd0afaaffe083038e9bb6660518c45
Kevin Pedro <kpedro88@gmail.com>
2020-02-21 17:35:34 -0600

    use fit extrapolation from CR

--- applyTF.py
+++ applyTF.py
@@ -1,12 +1,16 @@
 import sys
 from argparse import ArgumentParser, SUPPRESS
 
-def applyTF(iname,hname,fname,oname=""):
+def applyTF(iname,hname,fname,ename="",oname=""):
     from ROOT import TFile, TH1, TF1
     file = TFile.Open(iname)
     hist = file.Get(hname)
     hist.SetDirectory(0)
     fn = file.Get(fname)
+    if len(ename)>0:
+        efn = file.Get(ename)
+        for b in range(1,hist.GetNbinsX()+1):
+            hist.SetBinContent(b,efn.Eval(hist.GetBinCenter(b)))
     hist.Multiply(fn)
     if len(oname)==0:
         oname = iname.replace("tfs","applytfs")
@@ -23,9 +27,10 @@ if __name__=="__main__":
     parser.add_argument("-o","--output",type=str,default="",help="output file name")
     parser.add_argument("-h","--hist",type=str,required=True,help="histogram name")
     parser.add_argument("-f","--fit",type=str,required=True,help="fit function name")
+    parser.add_argument("-e","--extrap",type=str,default="",help="extrap function name")
     parser.add_argument("--help",action="help",default=SUPPRESS,help="Show this help message and exit.")
     args = parser.parse_args()
 
     sys.argv = []
 
-    applyTF(args.input,args.hist,args.fit,args.output)
+    applyTF(args.input,args.hist,args.fit,args.extrap,args.output)
