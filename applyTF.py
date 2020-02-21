f627494cb3692861b2310fcf4bff2e298145b766
Kevin Pedro <kpedro88@gmail.com>
2020-02-21 16:51:47 -0600

    save tf fits and test closure immediately

---
+++ applyTF.py
@@ -0,0 +1,31 @@
+import sys
+from argparse import ArgumentParser, SUPPRESS
+
+def applyTF(iname,hname,fname,oname=""):
+    from ROOT import TFile, TH1, TF1
+    file = TFile.Open(iname)
+    hist = file.Get(hname)
+    hist.SetDirectory(0)
+    fn = file.Get(fname)
+    hist.Multiply(fn)
+    if len(oname)==0:
+        oname = iname.replace("tfs","applytfs")
+    ofile = TFile.Open(oname,"RECREATE")
+    ofile.cd()
+    cr = fname.split("/")[0]
+    tname = "pred_"+hname
+    hist.Write(tname)
+    print(tname)
+
+if __name__=="__main__":
+    parser = ArgumentParser(add_help=False)
+    parser.add_argument("-i","--input",type=str,required=True,help="input file name")
+    parser.add_argument("-o","--output",type=str,default="",help="output file name")
+    parser.add_argument("-h","--hist",type=str,required=True,help="histogram name")
+    parser.add_argument("-f","--fit",type=str,required=True,help="fit function name")
+    parser.add_argument("--help",action="help",default=SUPPRESS,help="Show this help message and exit.")
+    args = parser.parse_args()
+
+    sys.argv = []
+
+    applyTF(args.input,args.hist,args.fit,args.output)
--- scripts/makeTFs.sh
+++ scripts/makeTFs.sh
@@ -19,8 +19,12 @@ for SETS in ${SETLIST[@]}; do
 #		HIST=input/input_svj_mt_hist_tf_fit_highdeta.txt
 #	fi
 	EXTRA="d:ratiomax[7]"
+	POLY="pol0"
 	if [[ "$SETS" == *"SVJ0" ]]; then
 		EXTRA="d:ratiomax[0.05]"
+		POLY="pol1"
 	fi
-	root -b -l -q 'KPlotDriver.C+(".",{"input/input_svj_stack_dijetmtdetahad_2017.txt","input/input_svj_mt_tf_fit.txt","'$HIST'","input/input_svj_ext_full_bkg.txt"},{"OPTION","'$EXTRA'","vs:chosensets['${SETS}']","vs:numers['${SETARR[0]}']","vs:denoms['${SETARR[1]}']","s+:printsuffix[_'${SETS}']","vs+:printformat[pdf]"},1)'
+	root -b -l -q 'KPlotDriver.C+(".",{"input/input_svj_stack_dijetmtdetahad_2017.txt","input/input_svj_mt_tf_fit.txt","'$HIST'","input/input_svj_ext_full_bkg.txt"},{"OPTION","'$EXTRA'","vs:chosensets['${SETS}']","vs:numers['${SETARR[0]}']","vs:denoms['${SETARR[1]}']","s+:printsuffix[_'${SETS}']","vs+:printformat[pdf]","s:rootfile[test/tfs_'${SETS}']"},1)'
+	python applyTF.py -i test/tfs_${SETS}.root -h MTAK8_${SETARR[1]} -f ${POLY}_MTAK8_ratio__${SETARR[0]}__${SETARR[1]}
+	root -b -l -q 'KPlotDriver.C+(".",{"input/input_svj_stack_dijetmtdetahad_2017.txt","input/input_svj_mt_tf_closure.txt","input/input_svj_mt_hist_full.txt","input/input_svj_ext_full_bkg_closure.txt"},{"OPTION","vs:chosensets['${SETS}']","vs:numers['${SETARR[0]}']","vs:denoms['${SETARR[1]}']","s+:printsuffix[_'${SETS}'_closure]","vs+:printformat[pdf]"},1)'
 done
