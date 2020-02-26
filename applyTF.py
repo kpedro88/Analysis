4be75bcf3f5acded74396b0306ccd955ca0e8e77
Kevin Pedro <kpedro88@gmail.com>
2020-02-26 08:10:16 -0600

    compute R for ABCD method

--- applyTF.py
+++ applyTF.py
@@ -1,7 +1,7 @@
 import sys
 from argparse import ArgumentParser, SUPPRESS
 
-def applyTF(iname,hname,fname,ename="",oname=""):
+def applyTF(iname,hname,fname,ename="",snames="",prename="",rname="",oname=""):
     from ROOT import TFile, TH1, TF1
     file = TFile.Open(iname)
     hist = file.Get(hname)
@@ -12,6 +12,24 @@ def applyTF(iname,hname,fname,ename="",oname=""):
         for b in range(1,hist.GetNbinsX()+1):
             hist.SetBinContent(b,efn.Eval(hist.GetBinCenter(b)))
     hist.Multiply(fn)
+
+    sets = []
+    if len(snames)>0:
+		rfile = TFile.Open(rname)
+        sets = snames.split(',')
+        hsets = []
+        for set in sets:
+            hsets.append(rfile.Get(prename+set))
+            hsets[-1].SetDirectory(0)
+        # compute R = (AD)/(CB)
+        hsets[3].Multiply(hsets[1])
+        hsets[2].Multiply(hsets[0])
+        hsets[3].Divide(hsets[2])
+        # pol1 fit to get R
+        pol1 = TF1("pol1","pol1",hist.GetXaxis().GetXmin(),hist.GetXaxis().GetXmax())
+        hsets[3].Fit(pol1,"N")
+        hist.Multiply(pol1)
+
     if len(oname)==0:
         oname = iname.replace("tfs","applytfs")
     ofile = TFile.Open(oname,"RECREATE")
@@ -28,9 +46,12 @@ if __name__=="__main__":
     parser.add_argument("-h","--hist",type=str,required=True,help="histogram name")
     parser.add_argument("-f","--fit",type=str,required=True,help="fit function name")
     parser.add_argument("-e","--extrap",type=str,default="",help="extrap function name")
+    parser.add_argument("-s","--sets",type=str,default="",help="comma-separated list of sets (B,D,C,A) for correl")
+    parser.add_argument("-p","--prefix",type=str,default="",help="prefix for correl")
+    parser.add_argument("-r","--rfile",type=str,default="",help="correl input file name")
     parser.add_argument("--help",action="help",default=SUPPRESS,help="Show this help message and exit.")
     args = parser.parse_args()
 
     sys.argv = []
 
-    applyTF(args.input,args.hist,args.fit,args.extrap,args.output)
+    applyTF(args.input,args.hist,args.fit,args.extrap,args.sets,args.prefix,args.rfile,args.output)
--- scripts/makeABCD.sh
+++ scripts/makeABCD.sh
@@ -6,17 +6,21 @@ runall() {
 	root -b -l -q 'KPlotDriver.C+(".",{"input/input_svj_stack_dijetmtdetahad_2017.txt","input/input_svj_mt_tf_fit.txt","input/input_svj_mt_hist_tf_fit.txt","'${SETFILE}'"},{"OPTION","'$EXTRA'","vs:chosensets['${SETARR[0]},${SETARR[1]}']","vs:numers['${SETARR[0]}']","vs:denoms['${SETARR[1]}']","s+:printsuffix[_'${SETARR[0]},${SETARR[1]}']","i:npanel[2]","b:balance_panels[1]","vs+:printformat[pdf]"},1)'
 	root -b -l -q 'KPlotDriver.C+(".",{"input/input_svj_stack_dijetmtdetahad_2017.txt","input/input_svj_mt_tf_fit.txt","input/input_svj_mt_hist_tf_fit.txt","'${SETFILE}'"},{"OPTION","'$EXTRA'","vs:chosensets['${SETARR[3]},${SETARR[2]}']","vs:numers['${SETARR[3]}']","vs:denoms['${SETARR[2]}']","s+:printsuffix[_'${SETARR[3]},${SETARR[2]}']","i:npanel[2]","b:balance_panels[1]","vs+:printformat[pdf]"},1)'
 	root -b -l -q 'KPlotDriver.C+(".",{"input/input_svj_stack_dijetmtdetahad_2017.txt","input/input_svj_mt_tf_fit.txt","input/input_svj_mt_hist_tf_fit.txt","'${SETFILE}'"},{"OPTION","'$EXTRA'","vs:chosensets['${SETS}']","vs:numers['${SETARR[0]}']","vs:denoms['${SETARR[1]}']","s+:printsuffix[_'${SETS}']","i:npanel[2]","b:balance_panels[1]","vs+:printformat[pdf]","s:rootfile[test/tfs_abcd_'${SETS}']"},0)'
+	# make separate root file for R
+	root -b -l -q 'KPlotDriver.C+(".",{"input/input_svj_stack_dijetmtdetahad_2017.txt","input/input_svj_mt_tf_fit.txt","input/input_svj_mt_hist_tf_fit.txt","'${MCSETFILE}'"},{"OPTION","'$EXTRA'","vs:chosensets['${SETS}']","vs:numers['${SETARR[0]}']","vs:denoms['${SETARR[1]}']","s+:printsuffix[_'${SETS}']","i:npanel[2]","b:balance_panels[1]","vs+:printformat[pdf]","s:rootfile[test/tfs_mc_abcd_'${SETS}']"},0)'
 #	for SET in ${SETARR[@]}; do
 #		root -b -l -q 'KPlotDriver.C+(".",{"input/input_svj_stack_dijetmtdetahad_2017.txt","input/input_svj_mt_tf_fit.txt","input/input_svj_mt_hist_tf_fit.txt","'${SETFILE}'"},{"OPTION","'$EXTRA'","vs:chosensets['${SET}']","vs:numers['${SET}']","vs:denoms['${SET}']","s+:printsuffix[_'${SET}'_res]","vs+:printformat[pdf]","s:ratiocalc[RelRes]","d:ratiomin[-0.5]","d:ratiomax[0.5]","s:rationame[residual (f-h)/f]"},1)'
 #	done
-	python applyTF.py -i test/tfs_abcd_${SETS}.root -h MTAK8_${SETARR[2]} -f ${POLY}_MTAK8_ratio__${SETARR[0]}__${SETARR[1]}
+	python applyTF.py -i test/tfs_abcd_${SETS}.root -h MTAK8_${SETARR[2]} -f ${POLY}_MTAK8_ratio__${SETARR[0]}__${SETARR[1]} -p MTAK8_ -s ${SETS} -r test/tfs_mc_abcd_${SETS}.root
 	root -b -l -q 'KPlotDriver.C+(".",{"input/input_svj_stack_dijetmtdetahad_2017.txt","input/input_svj_mt_tf_closure.txt","input/input_svj_mt_hist_full.txt","input/input_svj_ext_full_bkg_closure_abcd.txt"},{"OPTION","vs:chosensets['${SETARR[3]},${SETARR[3]}_${SETARR[2]}']","vs:numers['${SETARR[3]}']","vs:denoms['${SETARR[3]}_${SETARR[2]}']","s+:printsuffix[_'${SETS}'_closure]","vs+:printformat[pdf]"},1)'
 }
 
 TOYS=$1
-SETFILE=input/input_svj_ext_full_bkg.txt
+MCSETFILE=input/input_svj_ext_full_bkg.txt
 if [ -n "$TOYS" ]; then
 	SETFILE=input/input_svj_ext_full_bkg_toys.txt
+else
+	SETFILE=$MCSETFILE
 fi
 
 SETLIST=(
