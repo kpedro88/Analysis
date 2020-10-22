23b78b7c094e9d75b5a0d2b302edf5baddb366a7
Kevin Pedro <kpedro88@gmail.com>
2020-10-21 23:11:23 -0500

    simplify & extend plots, improve style

--- scripts/datacardPlots.sh
+++ scripts/datacardPlots.sh
@@ -10,15 +10,15 @@ SUFF=""
 MINPLOTS=""
 TOY=""
 SIGTOY=""
+SIGTOYNAME=""
 SETDATA=
 WINDOW=true
 NEWBINS=
 FITPLOT=""
-FITNAMETMP=""
-FITSETSIG=""
+FITFILE=""
 FITSETDAT=""
 
-while getopts "d:f:s:onymwbtTF" opt; do
+while getopts "d:f:s:onymwbtT:F" opt; do
 	case "$opt" in
 		d) EXTDIR=$OPTARG
 		;;
@@ -41,11 +41,10 @@ while getopts "d:f:s:onymwbtTF" opt; do
 		t) TOY=true
 		;;
 		T) SIGTOY=true
+		   SIGTOYNAME=$OPTARG
 		;;
 		F) FITPLOT=true
-		   HISTO=input/input_svj_mt_hist_full_fit.txt
-		   FITNAMETMP=input/input_svj_mt_fit_toy_REGION.txt
-		   FITSETSIG=SVJ_2100_20_0.3_peak,SVJ_3100_20_0.3_peak,SVJ_4100_20_0.3_peak
+		   FITFILE=input/input_svj_mt_fit_toy.txt
 		   FITSETDAT=data
 		;;
 	esac
@@ -64,24 +63,24 @@ if [ -n "$NORM" ]; then
 fi
 if [ -n "$YIELD" ]; then
 	EXTRA=',"b:yieldnorm[1]","s:yieldref[data]"'
-	SETDATA='"input/input_svj_ext_full_datacard_data.txt",'
+	SETDATA=input/input_svj_ext_full_datacard_data.txt
 fi
 if [ -n "$TOY" ]; then
 	if [ -n "$FITPLOT" ]; then
-		SETDATA='"input/input_svj_ext_full_datacard_toy_fit.txt"'
+		SETDATA=input/input_svj_ext_full_datacard_toy_fit.txt
 	else
-		SETDATA='"input/input_svj_ext_full_datacard_toy.txt",'
+		SETDATA=input/input_svj_ext_full_datacard_toy.txt
 	fi
 fi
 if [ -n "$SIGTOY" ]; then
+	EXTRA=',"s+:printsuffix[_'$SIGTOYNAME']"'
 	if [ -n "$FITPLOT" ]; then
-		SETDATA='"input/input_svj_ext_full_datacard_toy_fit.txt"'
-		FITSETDAT=data_sig
-		FITNAMETMP=$(echo $FITNAMETMP | sed 's/toy/sigtoy/')
-		EXTRA=',"s+:printsuffix[_sigtoy]"'
+		SETDATA=input/input_svj_ext_full_datacard_toy_fit.txt
+		FITSETDAT=data_$SIGTOYNAME
+		FITFILE=$(echo $FITFILE | sed 's/toy/'$SIGTOYNAME'/')
 	else
-		EXTRA=',"b:yieldnorm[1]"'
-		SETDATA='"input/input_svj_ext_full_datacard_sigtoy.txt",'
+		EXTRA="$EXTRA"',"b:yieldnorm[1]","s:yieldref[dnorm_'$SIGTOYNAME']","vs+:chosensets[data_'$SIGTOYNAME',data2_'$SIGTOYNAME',dnorm_'$SIGTOYNAME']"'
+		SETDATA=input/input_svj_ext_full_datacard_sigtoy.txt
 	fi
 fi
 if [ -n "$NEWBINS" ]; then
@@ -93,20 +92,20 @@ highCut \
 lowCut \
 highSVJ2 \
 lowSVJ2 \
-highSVJ0 \
-highSVJ1 \
-lowSVJ0 \
-lowSVJ1 \
+#highSVJ0 \
+#highSVJ1 \
+#lowSVJ0 \
+#lowSVJ1 \
 )
 RNAMES=(
 "high-^{}R_{T}" \
 "low-^{}R_{T}" \
 "high-SVJ2" \
 "low-SVJ2" \
-"high-SVJ0" \
-"high-SVJ1" \
-"low-SVJ0" \
-"low-SVJ1" \
+#"high-SVJ0" \
+#"high-SVJ1" \
+#"low-SVJ0" \
+#"low-SVJ1" \
 )
 
 MAXPLOTS=${#REGIONS[@]}
@@ -118,10 +117,9 @@ for ((i=0; i < $MAXPLOTS; i++)); do
 	REGION=${REGIONS[$i]}
 	RNAME=${RNAMES[$i]}
 	if [ -n "$FITPLOT" ]; then
-		FITFILE=$(echo $FITNAMETMP | sed 's/REGION/'$REGION'/')
-		root -b -l -q 'KPlotDriver.C+("'$EXTDIR'",{"input/input_svj_stack_dijetmtdetahad_2017.txt","'$FITFILE'","'$HISTO'",'"$SETDATA"'},{"OPTION","s:extfilename['${EXTFILE}']","s:exthisto_dir['${REGION}'_2018]","s+:printsuffix[_'${REGION}${SUFF}']","vs:extra_text['"${RNAME}"']","vs+:printformat[pdf]","vs:chosensets['$FITSETDAT,$FITSETSIG']","vs:numers['$FITSETDAT,$FITSETSIG']","vs:denoms['$FITSETDAT']"'"$EXTRA"'},1)'
+		root -b -l -q 'KPlotDriver.C+("'$EXTDIR'",{"input/input_svj_stack_dijetmtdetahad_2017.txt","'$FITFILE'","'$HISTO'","'$SETDATA'"},{"OPTION","s:extfilename['${EXTFILE}']","s:exthisto_dir['${REGION}'_2018]","s+:printsuffix[_'${REGION}${SUFF}']","vs:extra_text['"${RNAME}"']","vs+:printformat[pdf]","vs+:chosensets['$FITSETDAT']","vs+:numers['$FITSETDAT']","vs:denoms['$FITSETDAT']","vs:fits[fmain_'$REGION']"'"$EXTRA"'},1)'
 	else
-		root -b -l -q 'KPlotDriver.C+("'$EXTDIR'",{"input/input_svj_stack_dijetmtdetahad_2017.txt","input/input_svj_full_bdt_regions.txt","'$HISTO'",'"$SETDATA"'"input/input_svj_ext_full_datacard_regions'$SETSUFF'.txt"},{"OPTION","s:extfilename['${EXTFILE}']","s:exthisto_dir['${REGION}'_2018]","s+:printsuffix[_'${REGION}${SUFF}']","vs:extra_text['"${RNAME}"']","vs+:printformat[pdf]","vs:numers[]","vs:denoms[]"'"$EXTRA"'},1)'
+		root -b -l -q 'KPlotDriver.C+("'$EXTDIR'",{"input/input_svj_stack_dijetmtdetahad_2017.txt","input/input_svj_full_bdt_regions.txt","'$HISTO'","'$SETDATA'","input/input_svj_ext_full_datacard_regions'$SETSUFF'.txt"},{"OPTION","s:extfilename['${EXTFILE}']","s:exthisto_dir['${REGION}'_2018]","s+:printsuffix[_'${REGION}${SUFF}']","vs:extra_text['"${RNAME}"']","vs+:printformat[pdf]","vs:numers[]","vs:denoms[]"'"$EXTRA"'},1)'
 		if [ -n "$WINDOW" ]; then
 			echo "WINDOW: $EXTDIR $REGION"
 			python getWindowYield.py -f $EXTFILE -d ${REGION}_2018 -s SVJ_mZprime3100_mDark20_rinv03_alphapeak -b Bkg
