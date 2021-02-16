6b00c2b040c7bd3e21cc266420d123540b1d4a9a
Kevin Pedro <kpedro88@gmail.com>
2021-02-16 14:00:13 -0600

    input files for more bdt studies

---
+++ batch/HPsub_svj_BFKKM.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+source exportProd.sh
+
+JOBDIR=jobs
+INDIR=root://cmseos.fnal.gov//store/user/lpcsusyhad/SusyRA2Analysis2015/Run2ProductionV17
+CHECKARGS=""
+YEARS=()
+DRYRUN=""
+STYPE=""
+
+#check arguments
+while getopts "kdy:t:" opt; do
+	case "$opt" in
+		k) CHECKARGS="${CHECKARGS} -k"
+		;;
+		y) IFS="," read -a YEARS <<< "$OPTARG"
+		;;
+		t) STYPE="$OPTARG"
+		;;
+		d) DRYRUN="echo"
+		;;
+	esac
+done
+
+STORE=root://cmseos.fnal.gov//store/user/pedrok/SVJ2017/bfkkm/${STYPE}hist
+
+if [ "$STYPE" != q ] && [ "$STYPE" != s ]; then
+	echo "Unknown sample stype $STYPE"
+	exit 1
+fi
+
+./SKcheck.sh ${CHECKARGS}
+
+for YEAR in ${YEARS[@]}; do
+	INPUTS='"input/input_svj_discr_unskimmed.txt"'
+
+	SNAME=HistBFKKM${STYPE}${YEAR}
+	source export${SNAME}.sh
+
+	JOBNAME='svj_bfkkm'${STYPE}${YEAR}'_discr'
+	SLIST=${#SAMPLES[@]}
+	for ((i=0; i < ${#SAMPLES[@]}; i++)); do
+		SAMPLE=${SAMPLES[$i]}
+		SJOBNAME=${JOBNAME}'_part'${i}
+		OUTPUT='"OPTION","vstring:chosensets['${SAMPLE}']","string:rootfile['${SJOBNAME}']"'
+		echo 'KPlotDriver.C+("'"$INDIR"'",{'"$INPUTS"'},{'"$OUTPUT"'})' > jobs/input/macro_${SJOBNAME}.txt
+	done
+
+	$DRYRUN ./HPtemp.sh ${JOBDIR} ${INDIR} ${STORE} ${JOBNAME} "${SLIST}"
+done
---
+++ batch/exportHistBFKKMs2017.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+SAMPLES=(
+SVJ_1000_20_0.3_peak \
+SVJ_2000_20_0.3_peak \
+SVJ_3000_20_0.3_peak \
+SVJ_4000_20_0.3_peak \
+SVJ_BFKKM_modelA \
+SVJ_BFKKM_modelB \
+SVJ_1500_5_0.3_peak \
+SVJ_1500_20_0.3_low \
+SVJ_1500_20_0.5_peak \
+SVJ_1500_20_0.7_peak \
+)
