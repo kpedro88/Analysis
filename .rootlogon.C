f7c3002505b2b2f447715b86e448e0a2b964de19
Kevin Pedro <kpedro88@gmail.com>
2021-03-09 17:08:14 -0600

    fix for older versions of cling

--- .rootlogon.C
+++ .rootlogon.C
@@ -1,8 +1,10 @@
-#define LOCAL_ROOTLOGON
-
-#include "style-CMSTDR.C"
-
-void _rootlogon(){
+{
+  //processline is necessary because putting #include outside of function means the function must be named rather than anonymous
+  //and old versions of cling expect the function name to match the file name exactly, including the leading period, which is not allowed
+  //this leads to "warning: cannot find function '.rootlogon()'; falling back to .L" and then the function is not loaded properly
+  //(newer versions of cling check for _rootlogon instead to avoid this problem)
+  gInterpreter->ProcessLine("#define LOCAL_ROOTLOGON");
+  gInterpreter->ProcessLine("#include \"style-CMSTDR.C\"");
   setTDRStyle();
   gSystem->Load("libFWCoreFWLite.so");
   gSystem->Load("libDataFormatsFWLite.so");
