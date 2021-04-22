6a0a739452afaba8233808105c101e8859ab490f
Kevin Pedro <kpedro88@gmail.com>
2021-04-22 15:30:20 -0500

    make sure skim base and skimmer get constructed w/ full globalOpt

--- KCode/KSkimManager.h
+++ KCode/KSkimManager.h
@@ -32,20 +32,22 @@ class KSkimManager : public KManager {
 		//constructor
 		KSkimManager() : KManager(), setname(""), seltypes(""), outdir(""), MySkimmer(0), MyBase(0) {}
 		KSkimManager(string setname_, string seltypes_, string indir_, vector<string> input_, vector<string> direct_, string outdir_) : 
-			KManager(indir_), setname(setname_), seltypes(seltypes_), outdir(outdir_), MySkimmer(0), MyBase(0)
+			KManager(indir_), setname(setname_), seltypes(seltypes_), outdir(outdir_), MySkimmer(0), MyNamedBase(0), MyBase(0)
 		{
 			//parse most initializations based on text input
 			Initialize(input_,direct_);
 			
 			//safety checks
 			if(!parsed) return;
-			if(!MyBase){
+			if(!MyNamedBase){
 				stringstream ss;
 				KParser::printvec(input_,ss,",");
 				throw runtime_error("set "+setname+" was not found in "+ss.str()+"!");
 				parsed = false;
 				return;
 			}
+			MyBase = KParser::processBase(MyNamedBase,globalOpt);
+			MySkimmer = static_cast<KSkimmer*>(MyBase->GetLooper());
 			if(MyBase->GetTree()==NULL) { parsed = false; return; }
 		}
 		//destructor
@@ -56,8 +58,7 @@ class KSkimManager : public KManager {
 			KParser::process(line,'\t',fields);
 			//only process the set of interest
 			if(fields.size()>2 && fields[2]==setname) {
-				MyBase = KParser::processBase(line,globalOpt);
-				MySkimmer = static_cast<KSkimmer*>(MyBase->GetLooper());
+				MyNamedBase = KParser::processNamed<3>(line);
 			}
 		}
 		//where the magic happens
@@ -100,6 +101,7 @@ class KSkimManager : public KManager {
 		string setname, seltypes, outdir;
 		bool doClone;
 		KSkimmer* MySkimmer;
+		KNamedBase* MyNamedBase;
 		KBase* MyBase;
 };
 
