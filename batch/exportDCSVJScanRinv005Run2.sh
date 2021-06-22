a26a9db93f5eeccf7560f169113b10e1d7e2606d
Kevin Pedro <kpedro88@gmail.com>
2021-06-22 18:25:21 -0500

    update skim/DC input files for rinv = 0.05 scans

---
+++ batch/exportDCSVJScanRinv005Run2.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+export SAMPLES=(
+SVJ_1500_20_0.05_peak \
+SVJ_1700_20_0.05_peak \
+SVJ_1900_20_0.05_peak \
+SVJ_2100_20_0.05_peak \
+SVJ_2300_20_0.05_peak \
+SVJ_2500_20_0.05_peak \
+SVJ_2700_20_0.05_peak \
+SVJ_2900_20_0.05_peak \
+SVJ_3100_20_0.05_peak \
+SVJ_3300_20_0.05_peak \
+SVJ_3500_20_0.05_peak \
+SVJ_3700_20_0.05_peak \
+SVJ_3900_20_0.05_peak \
+SVJ_4100_20_0.05_peak \
+SVJ_4300_20_0.05_peak \
+SVJ_4500_20_0.05_peak \
+SVJ_4700_20_0.05_peak \
+SVJ_4900_20_0.05_peak \
+SVJ_5100_20_0.05_peak \
+)
