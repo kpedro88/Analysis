e63c84ecf925952ae021e3cc8f8371580bae36a0
Kevin Pedro <kpedro88@gmail.com>
2021-07-13 11:54:49 -0500

    input files for N-1 histograms

---
+++ batch/HPsub_svj_Nm1.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+source exportProd.sh
+
+JOBDIR=jobs
+INDIR=root://cmseos.fnal.gov//store/user/lpcsusyhad/SusyRA2Analysis2015/Run2ProductionV17
+STORE=root://cmseos.fnal.gov//store/user/pedrok/SVJ2017/Nm1/hist
+CHECKARGS=""
+YEARS=()
+TYPE=""
+DRYRUN=""
+MATCH=""
+
+#check arguments
+while getopts "kdy:t:m:" opt; do
+	case "$opt" in
+		k) CHECKARGS="${CHECKARGS} -k"
+		;;
+		y) IFS="," read -a YEARS <<< "$OPTARG"
+		;;
+		t) TYPE="$OPTARG"
+		;;
+		m) MATCH="$OPTARG"
+		;;
+		d) DRYRUN="echo"
+		;;
+	esac
+done
+
+./SKcheck.sh ${CHECKARGS}
+
+if [[ "$TYPE" == "scan" ]]; then
+	INDIR=root://cmseos.fnal.gov//store/user/lpcsusyhad/SVJ2017/Run2ProductionV17/scan
+fi
+
+for YEAR in ${YEARS[@]}; do
+	INPUTS='"input/input_svj_hp_Nm1_'${YEAR}'.txt","input/input_hp_sets_svj_'$TYPE'_'$YEAR'.txt"'
+
+	SNAME=HistSVJ${TYPE}Unskimmed${YEAR}
+	source export${SNAME}.sh
+
+	JOBNAME="svj_Nm1_${TYPE}_${YEAR}"
+	SLIST=${#SAMPLES[@]}
+	if [ -n "$MATCH" ]; then
+		SLIST=""
+	fi
+	for ((i=0; i < ${#SAMPLES[@]}; i++)); do
+		SAMPLE=${SAMPLES[$i]}
+		if [ -n "$MATCH" ]; then
+			if [[ "$SAMPLE" == *"$MATCH"* ]]; then
+				SLIST="$SLIST,$i"
+			else
+				continue
+			fi
+		fi
+		SJOBNAME=${JOBNAME}'_part'${i}
+		OUTPUT='"OPTION","vstring:chosensets['${SAMPLE}']","string:rootfile['${SJOBNAME}']"'
+		echo 'KPlotDriver.C+("'"$INDIR"'",{'"$INPUTS"'},{'"$OUTPUT"'})' > jobs/input/macro_${SJOBNAME}.txt
+	done
+	if [ -n "$MATCH" ]; then
+		# remove first char, prepend condor stuff
+		SLIST="Process in ${SLIST:1}"
+	fi
+
+	$DRYRUN ./HPtemp.sh ${JOBDIR} ${INDIR} ${STORE} ${JOBNAME} "${SLIST}"
+done
