73901ee1207d6985f2cf37952645e7ec0c3c70f6
Kevin Pedro <kpedro88@gmail.com>
2021-09-10 10:37:39 -0500

    update rinv005 scan files

--- input/dict_scan_svj_2016_rinv005.py
+++ input/dict_scan_svj_2016_rinv005.py
@@ -1,3 +1,3 @@
 flist = [
-[['PrivateSamples.MC2016_SVJ_ScanRinv005_2016_TuneCUETP8M1_13TeV_pythia8_n-1000'], ['SVJ_Zprime_ScanRinv005_MC2016']],
+[['PrivateSamples.MC2016_SVJ_ScanRinv005_2016_TuneCUETP8M1_13TeV_pythia8_n-500'], ['SVJ_Zprime_ScanRinv005_MC2016']],
 ]
