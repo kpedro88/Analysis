2e771d9ab6e7d84d62c5f9116e4433cc344ee2be
Kevin Pedro <kpedro88@gmail.com>
2021-11-30 18:43:49 -0600

    fix spacing for parser

--- scripts/selections_svj_supp.py
+++ scripts/selections_svj_supp.py
@@ -9,6 +9,6 @@ selections = OrderedDict([
     ('ElectronVeto', '\\neles & =0'),
     ('METFilter', '\\multicolumn{2}{c}{\\ptmiss filters}'),
     ('PhiSpikeVeto16', '\\DRphispike & >0.1'),
-    ('GapJetVeto', '\\text{veto}~f_{\\gamma}(j_{1})>0.7 &\\&~\\pt(j_{1}) > 1.0\\TeV'),
+    ('GapJetVeto', '\\text{veto}~f_{\\gamma}(j_{1})>0.7 & \\&~\\pt(j_{1}) > 1.0\\TeV'),
     ('DeltaPhiMinAK8', '\\mindphi & <0.8'),
 ])
