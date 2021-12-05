43220978b2ab8f467032bbf978c4ca95f233baae
Kevin Pedro <kpedro88@gmail.com>
2021-12-05 17:46:46 -0600

    fix jet symbols

--- scripts/selections_svj_supp.py
+++ scripts/selections_svj_supp.py
@@ -1,7 +1,7 @@
 from collections import OrderedDict
 
 selections = OrderedDict([
-    ('Dijet', '\\multicolumn{2}{c}{$\\pt(J_{1,2}) > 200\\GeV$, $\\eta(J_{1,2}) < 2.4$}'),
+    ('Dijet', '\\multicolumn{2}{c}{$\\pt(\\widejet_{1,2}) > 200\\GeV$, $\\eta(\\widejet_{1,2}) < 2.4$}'),
     ('METMTRatio', '\\metmt & >0.15'),
     ('DeltaEta', '\\deta & <1.5'),
     ('MTAK8', '\\mt & >1.5\\TeV'),
@@ -9,6 +9,6 @@ selections = OrderedDict([
     ('ElectronVeto', '\\neles & =0'),
     ('METFilter', '\\multicolumn{2}{c}{\\ptmiss filters}'),
     ('PhiSpikeVeto16', '\\DRphispike & >0.1'),
-    ('GapJetVeto', '\\text{veto}~f_{\\gamma}(j_{1})>0.7 & \\&~\\pt(j_{1}) > 1.0\\TeV'),
+    ('GapJetVeto', '\\text{veto}~f_{\\gamma}(\\narrowjet_{1})>0.7 & \\&~\\pt(\\narrowjet_{1}) > 1.0\\TeV'),
     ('DeltaPhiMinAK8', '\\mindphi & <0.8'),
 ])
