770d209ed819dc871300c71c37d810adcc5373cd
Kevin Pedro <kpedro88@gmail.com>
2024-06-16 14:57:41 -0500

    add new samples

--- input/dict_skim_pmssm_2017.py
+++ input/dict_skim_pmssm_2017.py
@@ -1,10 +1,14 @@
 flist = [
-[['Fall17Fast.PMSSM_set_1_LL_TuneCP2_13TeV-pythia8'],['pMSSM_set1LL_MC2017']],
+[['Fall17Fast.PMSSM_set_1_LL_TuneCP2_13TeV-pythia8',
+  'Fall17Fast.PMSSM_set_1_LL_TuneCP2_13TeV-pythia8_v3',
+  'Fall17Fast.PMSSM_set_1_LL_TuneCP2_13TeV-pythia8_ext1'],['pMSSM_set1LL_MC2017']],
 [['Fall17Fast.PMSSM_set_1_prompt_1_TuneCP2_13TeV-pythia8'],['pMSSM_set1prompt1_MC2017']],
 [['Fall17Fast.PMSSM_set_1_prompt_2_TuneCP2_13TeV-pythia8'],['pMSSM_set1prompt2_MC2017']],
-[['Fall17Fast.PMSSM_set_1_prompt_3_TuneCP2_13TeV-pythia8'],['pMSSM_set1prompt3_MC2017']],
+[['Fall17Fast.PMSSM_set_1_prompt_3_TuneCP2_13TeV-pythia8',
+  'Fall17Fast.PMSSM_set_1_prompt_3_TuneCP2_13TeV-pythia8_v3'],['pMSSM_set1prompt3_MC2017']],
 [['Fall17Fast.PMSSM_set_2_LL_1_TuneCP2_13TeV-pythia8'],['pMSSM_set2LL1_MC2017']],
-[['Fall17Fast.PMSSM_set_2_LL_2_TuneCP2_13TeV-pythia8'],['pMSSM_set2LL2_MC2017']],
+[['Fall17Fast.PMSSM_set_2_LL_2_TuneCP2_13TeV-pythia8',
+  'Fall17Fast.PMSSM_set_2_LL_2_TuneCP2_13TeV-pythia8_v3'],['pMSSM_set2LL2_MC2017']],
 [['Fall17Fast.PMSSM_set_2_prompt_1_TuneCP2_13TeV-pythia8'],['pMSSM_set2prompt1_MC2017']],
 [['Fall17Fast.PMSSM_set_2_prompt_2_TuneCP2_13TeV-pythia8'],['pMSSM_set2prompt2_MC2017']],
 ]
